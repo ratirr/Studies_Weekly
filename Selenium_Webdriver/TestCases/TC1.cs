e5d6e01e1952e88e55e0cd68d1023accce597407
ratirr <45217104+ratirr@users.noreply.github.com>
2018-11-26 12:46:33 -0700

    1.7

--- Selenium_Webdriver/TestCases/TC1.cs
+++ Selenium_Webdriver/TestCases/TC1.cs
@@ -31,55 +31,60 @@ namespace Selenium_Webdriver.TestCases
 
             //TC2
 
+            //IWebDriver driver = new ChromeDriver();
+            //driver.Url = "https://app.studiesweekly.com/online/";
+
+            //// Store the parent window into a variable for further use 
+            //String parentWindowHandle = driver.CurrentWindowHandle;
+            //Console.WriteLine("Parent window's handle -> " + parentWindowHandle);
+
+            //IWebElement clickElement = driver.FindElement(By.Id("cleverLogin"));
+            ////I am using 'for' loop to get multiple windows by clicking the element
+            //for (var i = 0; i < 1; i++)
+            //{
+            //    clickElement.Click();
+
+            //}
+
+
+            //List<string> lstWindow = driver.WindowHandles.ToList();
+            //String lastWindowHandle = "";
+            //foreach (var handle in lstWindow)
+            //{
+            //    Console.WriteLine("Switching to window - > " + handle);
+            //    Console.WriteLine("Navigating to Login Using \"Clever\"");
+
+            //    //Switch to the desired window first and then execute commands using driver
+            //    driver.SwitchTo().Window(handle);
+
+            //    driver.Navigate().GoToUrl("https://clever.com/oauth/district-picker?response_type=code&redirect_uri=https%3A%2F%2Fapp.studiesweekly.com%2Fonline%2Fclever&client_id=907888321594e36e4579");
+            //    lastWindowHandle = handle;
+            //}
+
+            ////Switch to the parent window
+            //driver.SwitchTo().Window(parentWindowHandle);
+
+            ////close the parent window
+            //driver.Close();
+
+            ////at this point there is no focused window, we have to explicitly switch back to some window.
+            //driver.SwitchTo().Window(lastWindowHandle);
+
+            //driver.Url = "https://app.studiesweekly.com/online/";
+
+
+            //Tc3 One complete registration
+
             IWebDriver driver = new ChromeDriver();
-            driver.Url = "https://app.studiesweekly.com/online/";
-
-            // Store the parent window into a variable for further use 
-            String parentWindowHandle = driver.CurrentWindowHandle;
-            Console.WriteLine("Parent window's handle -> " + parentWindowHandle);
-
-            IWebElement clickElement = driver.FindElement(By.Id("cleverLogin"));
-            //I am using 'for' loop to get multiple windows by clicking the element
-            for (var i = 0; i < 1; i++)
-            {
-                clickElement.Click();
-
-            }
-
-            /*
-            * driver.WindowHandles is a ReadOnlycollection So i am using '.ToList()' and store into the 'List<string>'
-            * Again using 'for loop' to traverse all window which are opened by the above loop 
-            * then i use '.SwitchTo().Window'. Basically this is use to switch your control from parent window to current window
-            **/
-
-            List<string> lstWindow = driver.WindowHandles.ToList();
-            String lastWindowHandle = "";
-            foreach (var handle in lstWindow)
-            {
-                Console.WriteLine("Switching to window - > " + handle);
-                Console.WriteLine("Navigating to google.com");
-
-                //Switch to the desired window first and then execute commands using driver
-                driver.SwitchTo().Window(handle);
-
-                driver.Navigate().GoToUrl("https://clever.com/oauth/district-picker?response_type=code&redirect_uri=https%3A%2F%2Fapp.studiesweekly.com%2Fonline%2Fclever&client_id=907888321594e36e4579");
-                lastWindowHandle = handle;
-            }
-
-            //Switch to the parent window
-            driver.SwitchTo().Window(parentWindowHandle);
-
-            //close the parent window
-            driver.Close();
-
-            //at this point there is no focused window, we have to explicitly switch back to some window.
-            driver.SwitchTo().Window(lastWindowHandle);
 
             driver.Url = "https://app.studiesweekly.com/online/";
 
-
-
-
+            IWebElement reg = driver.FindElement(By.XPath("//a[@href='https://app.studiesweekly.com/online/users/register_parent']"));
+            reg.Click();
+            IWebElement a = driver.FindElement(By.XPath("//input[@id='firstName'][@ng-model='user.firstName']"));
+            a.SendKeys("Rati");
+            IWebElement b = driver.FindElement(By.XPath("//input[@id='lastName'][@name='lastname']"));
+            b.SendKeys("Rajan");
 
 
         }
