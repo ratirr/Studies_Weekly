d99f8cf7f2b6f0155f885242e8c8c7fd0911c1e0
ratirr <45217104+ratirr@users.noreply.github.com>
2018-11-28 06:47:09 -0700

    1.9

--- Selenium_Webdriver/TestCases/TC1.cs
+++ Selenium_Webdriver/TestCases/TC1.cs
@@ -8,109 +8,121 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
+using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace Selenium_Webdriver.TestCases
 {
     class TC1
     {
+
+        public DB_Connection()
+        {
+            //SqlConnection conn = new SqlConnection("Server=.\\LAPTOP-V0QHNVJR;Database=MortgageHouse;Integrated Security=true");
+            SqlConnection conn = new SqlConnection("Data Source=(local);Initial Catalog=MortgageHouse;Integrated Security=true");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT Effective , Term, Loan from dbo.All_Home", conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("{1},{0}", reader.GetString(0), reader.GetString(1));
+            }
+            reader.Close();
+            conn.Close();
+
+            if (Debugger.IsAttached)
+            {
+                Console.ReadLine();
+            }
+        }
+
         public static void Main(String[] args)
         {
-            //IWebDriver driver = new ChromeDriver();
-            //driver.Url = ("https://app.studiesweekly.com/online/");
-            //string Title_Page = driver.Title;
-            //Console.WriteLine("Title of the page is {0}", Title_Page);
-            //int title_length = driver.Title.Length;
-            //Console.WriteLine("Length of the title : {0}",title_length);
-            //string page_source = driver.PageSource;
-            //Console.WriteLine("Length of the title : {0}", page_source);
-            //int pg_length = driver.PageSource.Length;
-            //Console.WriteLine("Length of the title : {0}", pg_length);
-            //string url_page = driver.Url;
-            //Console.WriteLine("Url is: {0}", url_page);
-            //int URL_length = driver.Url.Length;
-            //Console.WriteLine("Length of the title : {0}", URL_length);
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = ("https://app.studiesweekly.com/online/");
+            string Title_Page = driver.Title;
+            Console.WriteLine("Title of the page is {0}", Title_Page);
+            int title_length = driver.Title.Length;
+            Console.WriteLine("Length of the title : {0}", title_length);
+            string page_source = driver.PageSource;
+            Console.WriteLine("Length of the title : {0}", page_source);
+            int pg_length = driver.PageSource.Length;
+            Console.WriteLine("Length of the title : {0}", pg_length);
+            string url_page = driver.Url;
+            Console.WriteLine("Url is: {0}", url_page);
+            int URL_length = driver.Url.Length;
+            Console.WriteLine("Length of the title : {0}", URL_length);
 
 
             //TC2
 
-            //IWebDriver driver = new ChromeDriver();
-            //driver.Url = "https://app.studiesweekly.com/online/";
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://app.studiesweekly.com/online/";
 
-            //// Store the parent window into a variable for further use 
-            //String parentWindowHandle = driver.CurrentWindowHandle;
-            //Console.WriteLine("Parent window's handle -> " + parentWindowHandle);
+            // Store the parent window into a variable for further use 
+            String parentWindowHandle = driver.CurrentWindowHandle;
+            Console.WriteLine("Parent window's handle -> " + parentWindowHandle);
 
-            //IWebElement clickElement = driver.FindElement(By.Id("cleverLogin"));
-            ////I am using 'for' loop to get multiple windows by clicking the element
-            //for (var i = 0; i < 1; i++)
-            //{
-            //    clickElement.Click();
+            IWebElement clickElement = driver.FindElement(By.Id("cleverLogin"));
+            //I am using 'for' loop to get multiple windows by clicking the element
+            for (var i = 0; i < 1; i++)
+            {
+                clickElement.Click();
 
-            //}
+            }
 
 
-            //List<string> lstWindow = driver.WindowHandles.ToList();
-            //String lastWindowHandle = "";
-            //foreach (var handle in lstWindow)
-            //{
-            //    Console.WriteLine("Switching to window - > " + handle);
-            //    Console.WriteLine("Navigating to Login Using \"Clever\"");
+            List<string> lstWindow = driver.WindowHandles.ToList();
+            String lastWindowHandle = "";
+            foreach (var handle in lstWindow)
+            {
+                Console.WriteLine("Switching to window - > " + handle);
+                Console.WriteLine("Navigating to Login Using \"Clever\"");
 
-            //    //Switch to the desired window first and then execute commands using driver
-            //    driver.SwitchTo().Window(handle);
+                //Switch to the desired window first and then execute commands using driver
+                driver.SwitchTo().Window(handle);
 
-            //    driver.Navigate().GoToUrl("https://clever.com/oauth/district-picker?response_type=code&redirect_uri=https%3A%2F%2Fapp.studiesweekly.com%2Fonline%2Fclever&client_id=907888321594e36e4579");
-            //    lastWindowHandle = handle;
-            //}
+                driver.Navigate().GoToUrl("https://clever.com/oauth/district-picker?response_type=code&redirect_uri=https%3A%2F%2Fapp.studiesweekly.com%2Fonline%2Fclever&client_id=907888321594e36e4579");
+                lastWindowHandle = handle;
+            }
 
-            ////Switch to the parent window
-            //driver.SwitchTo().Window(parentWindowHandle);
+            //Switch to the parent window
+            driver.SwitchTo().Window(parentWindowHandle);
 
-            ////close the parent window
-            //driver.Close();
+            //close the parent window
+            driver.Close();
 
-            ////at this point there is no focused window, we have to explicitly switch back to some window.
-            //driver.SwitchTo().Window(lastWindowHandle);
+            //at this point there is no focused window, we have to explicitly switch back to some window.
+            driver.SwitchTo().Window(lastWindowHandle);
 
-            //driver.Url = "https://app.studiesweekly.com/online/";
+            driver.Url = "https://app.studiesweekly.com/online/";
 
 
             //Tc3 One complete registration
 
-            //IWebDriver driver = new ChromeDriver();
-
-            //driver.Url = "https://app.studiesweekly.com/online/";
-
-            //IWebElement reg = driver.FindElement(By.XPath("//a[@href='https://app.studiesweekly.com/online/users/register_parent']"));
-            //reg.Click();
-            ////IWebElement a = driver.FindElement(By.XPath("//input[@id='firstName'][@ng-model='user.firstName']")).SendKeys("Rati");
-            ////IWebElement b = driver.FindElement(By.XPath("//input[@id='lastName'][@name='lastname']"));
-            ////b.SendKeys("Rajan");
-            //driver.FindElement(By.Id("firstName")).SendKeys("Rati");
-
-
             IWebDriver driver = new ChromeDriver();
+
             driver.Url = "https://app.studiesweekly.com/online/";
 
-            IWebElement un = driver.FindElement(By.XPath("//input[@id='username'][@class='form-control input-lg']"));
-            un.SendKeys("rati.rajan18@gmail.com");
-            IWebElement pwd = driver.FindElement(By.XPath("//input[@id='password'][@class='form-control input-lg']"));
-            pwd.SendKeys("Ringer123");
-            driver.FindElement(By.Id("loginFormSubmit")).Click();
+            IWebElement reg = driver.FindElement(By.XPath("//a[@href='https://app.studiesweekly.com/online/users/register_parent']"));
+            reg.Click();
+            //IWebElement a = driver.FindElement(By.XPath("//input[@id='firstName'][@ng-model='user.firstName']")).SendKeys("Rati");
+            //IWebElement b = driver.FindElement(By.XPath("//input[@id='lastName'][@name='lastname']"));
+            //b.SendKeys("Rajan");
+            driver.FindElement(By.Id("firstName")).SendKeys("Rati");
 
-            INavigation webElement = driver.Navigate().Forward("https://app.studiesweekly.com/online/parents"); 
 
-            Assert.That(INavigation, webElement);
 
-            //var homePage = new HomePage();
-            //PageFactory.InitElements(driver, homePage);
-            //homePage.MyAccount.Click();
 
-            //var loginPage = new LoginPage();
-            //PageFactory.InitElements(driver, loginPage);
-            //loginPage.UserName.SendKeys("TestUser_1");
-            //loginPage.Password.SendKeys("Test@123");
-            //loginPage.Submit.Submit();
+            var homePage = new HomePage();
+            PageFactory.InitElements(driver, homePage);
+            homePage.MyAccount.Click();
+
+            var loginPage = new LoginPage();
+            PageFactory.InitElements(driver, loginPage);
+            loginPage.UserName.SendKeys("TestUser_1");
+            loginPage.Password.SendKeys("Test@123");
+            loginPage.Submit.Submit();
 
 
         }
--- Selenium_Webdriver/UnitTestCases/tc1_unit.cs
+++ Selenium_Webdriver/UnitTestCases/tc1_unit.cs
@@ -1,118 +1,118 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
-using NUnit.Framework;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using OpenQA.Selenium;
+//using OpenQA.Selenium.Chrome;
+//using OpenQA.Selenium.Firefox;
+//using NUnit.Framework;
 
-namespace Selenium_Webdriver.UnitTestCases
+//namespace Selenium_Webdriver.UnitTestCases
 
-{
-    class tc1_unit
-    {
-        IWebDriver driver = new ChromeDriver();
-        [SetUp]
-        public void start()
-        {
-            driver.Url = ("https://app.studiesweekly.com/online/");
-        }
+//{
+//    class tc1_unit
+//    {
+//        IWebDriver driver = new ChromeDriver();
+//        [SetUp]
+//        public void start()
+//        {
+//            driver.Url = ("https://app.studiesweekly.com/online/");
+//        }
 
-        //TC1 : Validates websites title, pagesource and url as pre requisite for testing
-        [Test]
-        public void test1()
-        {
-            int title_length = driver.Title.Length;
-            string a = title_length.ToString();
-            System.Diagnostics.Debug.Write("Length of the title : {0}", a);
-            string page_source = driver.PageSource;
-            System.Diagnostics.Debug.Write("Length of the title : {0}", page_source);
-            int pg_length = driver.PageSource.Length;
-            string b = title_length.ToString();
-            System.Diagnostics.Debug.Write("Length of the title : {0}", b);
-            string url_page = driver.Url;
-            System.Diagnostics.Debug.Write("Url is: {0}", url_page);
-            int URL_length = driver.Url.Length;
-            string c = title_length.ToString();
-            System.Diagnostics.Debug.Write("Length of the title : {0}", c);
-        }
+//        //TC1 : Validates websites title, pagesource and url as pre requisite for testing
+//        [Test]
+//        public void test1()
+//        {
+//            int title_length = driver.Title.Length;
+//            string a = title_length.ToString();
+//            System.Diagnostics.Debug.Write("Length of the title : {0}", a);
+//            string page_source = driver.PageSource;
+//            System.Diagnostics.Debug.Write("Length of the title : {0}", page_source);
+//            int pg_length = driver.PageSource.Length;
+//            string b = title_length.ToString();
+//            System.Diagnostics.Debug.Write("Length of the title : {0}", b);
+//            string url_page = driver.Url;
+//            System.Diagnostics.Debug.Write("Url is: {0}", url_page);
+//            int URL_length = driver.Url.Length;
+//            string c = title_length.ToString();
+//            System.Diagnostics.Debug.Write("Length of the title : {0}", c);
+//        }
 
-        //TC2 : Checks navigation irrespective elements dynamically 
-        [Test]
-        public void test2()
-        {
-            IWebDriver driver = new FirefoxDriver();
-            driver.Url = "https://app.studiesweekly.com/online/";
+//        //TC2 : Checks navigation irrespective elements dynamically 
+//        [Test]
+//        public void test2()
+//        {
+//            IWebDriver driver = new FirefoxDriver();
+//            driver.Url = "https://app.studiesweekly.com/online/";
 
-            // Store the parent window into a variable for further use 
-            String parentWindowHandle = driver.CurrentWindowHandle;
-            Console.WriteLine("Parent window's handle -> " + parentWindowHandle);
+//            // Store the parent window into a variable for further use 
+//            String parentWindowHandle = driver.CurrentWindowHandle;
+//            Console.WriteLine("Parent window's handle -> " + parentWindowHandle);
 
-            IWebElement clickElement = driver.FindElement(By.Id("cleverLogin"));
-            //I am using 'for' loop to get multiple windows by clicking the element
-            for (var i = 0; i < 1; i++)
-            {
-                clickElement.Click();
+//            IWebElement clickElement = driver.FindElement(By.Id("cleverLogin"));
+//            //I am using 'for' loop to get multiple windows by clicking the element
+//            for (var i = 0; i < 1; i++)
+//            {
+//                clickElement.Click();
 
-            }
+//            }
 
-            /*
-            * driver.WindowHandles is a ReadOnlycollection So i am using '.ToList()' and store into the 'List<string>'
-            * Again using 'for loop' to traverse all window which are opened by the above loop 
-            * then i use '.SwitchTo().Window'. Basically this is use to switch your control from parent window to current window
-            **/
+//            /*
+//            * driver.WindowHandles is a ReadOnlycollection So i am using '.ToList()' and store into the 'List<string>'
+//            * Again using 'for loop' to traverse all window which are opened by the above loop 
+//            * then i use '.SwitchTo().Window'. Basically this is use to switch your control from parent window to current window
+//            **/
 
-            List<string> lstWindow = driver.WindowHandles.ToList();
-            String lastWindowHandle = "";
-            foreach (var handle in lstWindow)
-            {
-                Console.WriteLine("Switching to window - > " + handle);
-                Console.WriteLine("Navigating to google.com");
+//            List<string> lstWindow = driver.WindowHandles.ToList();
+//            String lastWindowHandle = "";
+//            foreach (var handle in lstWindow)
+//            {
+//                Console.WriteLine("Switching to window - > " + handle);
+//                Console.WriteLine("Navigating to google.com");
 
-                //Switch to the desired window first and then execute commands using driver
-                driver.SwitchTo().Window(handle);
+//                //Switch to the desired window first and then execute commands using driver
+//                driver.SwitchTo().Window(handle);
 
-                driver.Navigate().GoToUrl("https://clever.com/oauth/district-picker?response_type=code&redirect_uri=https%3A%2F%2Fapp.studiesweekly.com%2Fonline%2Fclever&client_id=907888321594e36e4579");
-                lastWindowHandle = handle;
-            }
+//                driver.Navigate().GoToUrl("https://clever.com/oauth/district-picker?response_type=code&redirect_uri=https%3A%2F%2Fapp.studiesweekly.com%2Fonline%2Fclever&client_id=907888321594e36e4579");
+//                lastWindowHandle = handle;
+//            }
 
-            //Switch to the parent window
-            driver.SwitchTo().Window(parentWindowHandle);
+//            //Switch to the parent window
+//            driver.SwitchTo().Window(parentWindowHandle);
 
-            //close the parent window
-            driver.Close();
+//            //close the parent window
+//            driver.Close();
 
-            //at this point there is no focused window, we have to explicitly switch back to some window.
-            driver.SwitchTo().Window(lastWindowHandle);
+//            //at this point there is no focused window, we have to explicitly switch back to some window.
+//            driver.SwitchTo().Window(lastWindowHandle);
 
-            driver.Url = "https://app.studiesweekly.com/online/";
+//            driver.Url = "https://app.studiesweekly.com/online/";
 
-        }
+//        }
 
-        //TC3: Login and Assertion
-        [Test]
+//        //TC3: Login and Assertion
+//        [Test]
 
-        class void login
-        { 
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "https://app.studiesweekly.com/online/";
+//        class void login
+//        { 
+//            IWebDriver driver = new ChromeDriver();
+//            driver.Url = "https://app.studiesweekly.com/online/";
 
-            IWebElement un = driver.FindElement(By.XPath("//input[@id='username'][@class='form-control input-lg']"));
-            un.SendKeys("rati.rajan18@gmail.com");
-            IWebElement pwd = driver.FindElement(By.XPath("//input[@id='password'][@class='form-control input-lg']"));
-            pwd.SendKeys("Ringer123");
-            driver.FindElement(By.Id("loginFormSubmit")).Click();
+//            IWebElement un = driver.FindElement(By.XPath("//input[@id='username'][@class='form-control input-lg']"));
+//            un.SendKeys("rati.rajan18@gmail.com");
+//            IWebElement pwd = driver.FindElement(By.XPath("//input[@id='password'][@class='form-control input-lg']"));
+//            pwd.SendKeys("Ringer123");
+//            driver.FindElement(By.Id("loginFormSubmit")).Click();
 
-             INavigation webElement = driver.Navigate().Forward("https://app.studiesweekly.com/online/parents");
+//             INavigation webElement = driver.Navigate().Forward("https://app.studiesweekly.com/online/parents");
 
-             Assert.That(INavigation, webElement);
+//             Assert.That(INavigation, webElement);
 
            
-         }
+//         }
 
-    }
+//    }
 
 
-}
+//}
