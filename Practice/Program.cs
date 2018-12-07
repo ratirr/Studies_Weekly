94aefef93227bc4b705dbddaa813a61cfc8c6445
ratirr <45217104+ratirr@users.noreply.github.com>
2018-12-06 20:10:39 -0700

    2.2

---
+++ Practice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int? EntryAllowed = null;
+
+            int NumberOfGuests = 10;
+
+            if (EntryAllowed == null)
+            {
+                Console.WriteLine("No one is allowed at the moment");
+            }
+            else if (EntryAllowed != null)
+            {
+                NumberOfGuests = (int)EntryAllowed;
+                Console.WriteLine("We can allow {0}", NumberOfGuests);
+            }
+
+
+        }
+    }
+}
